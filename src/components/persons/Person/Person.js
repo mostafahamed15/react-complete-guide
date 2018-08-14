4b920bd7ff5997b2773d038b52d8469f2306a835
Mostafa Hamed <mostafahamed73@yahoo.com>
2018-08-14 19:47:53 +0200

    Converting stateless to statefull component

--- src/components/persons/Person/Person.js
+++ src/components/persons/Person/Person.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './person.css';
-const person = (props)=>{
-  
-    return (
-        <div className = {classes.Person} >
-    <p onClick = {props.click}>I'm a {props.name} and I'm {props.age} years old .</p>
-    <p > {props.children} </p>
-    <input type = 'text' onChange = {props.changed} value = {props.name} />
-    {/* value = {props.name} donot work i donot know why make it */}
-    </div>
-)
-};
-export default person;
+class Person  extends Component {
+    state = {  }
+    render() { 
+        return (  
+        
+                <div className = {classes.Person} >
+            <p onClick = {this.props.click}>I'm a {this.props.name} and I'm {this.props.age} years old .</p>
+            <p > {this.props.children} </p>
+            <input type = 'text' onChange = {this.props.changed} value = {this.props.name} />
+            {/* value = {props.name} donot work i donot know why make it */}
+            </div>
+        
+        );
+    }
+}
+ 
+export default Person ;
+
--- src/components/persons/persons.js
+++ src/components/persons/persons.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
-const persons = (props) => props.persons.map((person, index) => {
-        return (
+class Persons extends Component {
+    state = {  }
+    render() { 
+        return this.props.persons.map((person, index) => {
+                return (
+                
+                <Person 
+                click={() => this.props.clicked(index)}
+                changed = {(event) => this.props.changed(event, person.id)}
+                name = {person.name}
+                age = {person.age} 
+                key = {person.id}
+               />
+               
+            )}); 
         
-        <Person 
-        click={() => props.clicked(index)}
-        changed = {(event) => props.changed(event, person.id)}
-        name = {person.name}
-        age = {person.age} 
-        key = {person.id}
-       />
-       
-    )})
-export default persons;
+    }
+}
+ 
+export default Persons ;
