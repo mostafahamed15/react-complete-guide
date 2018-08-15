0bf63f93f4bfb4979e1ee73c52f582ac1a1b19ed
Mostafa Hamed <mostafahamed73@yahoo.com>
2018-08-15 10:49:03 +0200

    Component updating life cycle in action

--- src/components/persons/persons.js
+++ src/components/persons/persons.js
@@ -14,6 +14,21 @@ class Persons extends Component {
       componentDidMount(){
         console.log('[persons.js] inside componentDidMount()');
       }
+      componentWillReceiveProps(nextProps){
+          console.log('[Update persons.js] inside componentWillReceiveProps()', nextProps);
+      }
+
+      shouldComponentUpdate(nextProps, nextState){
+          console.log('[Update persons.js] inside shouldComponentUpdate()', nextProps, nextState);
+          return nextProps.persons !== this.props.persons;
+      }
+
+      componentWillUpdate(nextProps, nextState){
+        console.log('[Update persons.js] inside componentWillUpdate()', nextProps, nextState);
+      }
+      componentDidUpdate(){
+        console.log('[Update persons.js] inside componentDidUpdate()');
+      }
     state = {  }
     render() { 
         console.log('[persons.js] inside rendrer()');
